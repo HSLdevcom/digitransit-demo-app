afabf9f3d723c9ef97f3de43b966b8e6627ceaa3
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-07-09 13:56:08 +0300

    location request in MainActivity

--- app/src/main/java/fi/hsl/demoapp/MainActivity.java
+++ app/src/main/java/fi/hsl/demoapp/MainActivity.java
@@ -1,14 +1,58 @@
 package fi.hsl.demoapp;
 
+import android.Manifest;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LOCATION_PERMISSION_REQUEST = 1;
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getLocation().observe(this, new Observer<Location>() {
+            @Override
+            public void onChanged(@Nullable Location location) {
+                Toast.makeText(MainActivity.this, location.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        if (!viewModel.getLocation().isLocationRequested()) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                viewModel.getLocation().requestLocation();
+            } else {
+                requestPermissions(new String[]{ Manifest.permission.ACCESS_FINE_LOCATION }, LOCATION_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case LOCATION_PERMISSION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    viewModel.getLocation().requestLocation();
+                } else {
+                    //TODO: handle permission denial
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
