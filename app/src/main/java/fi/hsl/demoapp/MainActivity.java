90f0d1e4ada39406ac02e0d3bb6803d6065a9714
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-07-09 15:42:51 +0300

    recyclerview for stops

--- app/src/main/java/fi/hsl/demoapp/MainActivity.java
+++ app/src/main/java/fi/hsl/demoapp/MainActivity.java
@@ -6,12 +6,20 @@ import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,18 +30,25 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewModel;
 
+    private RecyclerView stopsView;
+    private StopAdapter stopsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        stopsView = findViewById(R.id.stops);
+        stopsView.setHasFixedSize(true);
+        stopsView.setLayoutManager(new LinearLayoutManager(this));
+        stopsAdapter = new StopAdapter();
+        stopsView.setAdapter(stopsAdapter);
+
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         viewModel.getStops().observe(this, new Observer<List<StopAtDistance>>() {
             @Override
             public void onChanged(@Nullable List<StopAtDistance> stopAtDistances) {
-                for (StopAtDistance stopAtDistance : stopAtDistances) {
-                    Log.i("MainActivity", stopAtDistance.getStop().getName()+" - "+stopAtDistance.getDistance()+"m");
-                }
+                stopsAdapter.setData(stopAtDistances);
             }
         });
 
@@ -58,6 +73,47 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 break;
+        }
+    }
+
+    private static class StopAdapter extends RecyclerView.Adapter<StopAdapter.StopViewHolder> {
+        private List<StopAtDistance> stops = new ArrayList<>();
+
+        public void setData(List<StopAtDistance> stops) {
+            this.stops.clear();
+            this.stops.addAll(stops);
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public StopViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new StopViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.stop, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull StopViewHolder holder, int position) {
+            StopAtDistance stopAtDistance = stops.get(position);
+
+            holder.name.setText(stopAtDistance.getStop().getName());
+            holder.distance.setText(stopAtDistance.getDistance()+"m");
+        }
+
+        @Override
+        public int getItemCount() {
+            return stops.size();
+        }
+
+        public static class StopViewHolder extends RecyclerView.ViewHolder {
+            public final TextView name;
+            public final TextView distance;
+
+            public StopViewHolder(View itemView) {
+                super(itemView);
+
+                name = itemView.findViewById(R.id.name);
+                distance = itemView.findViewById(R.id.distance);
+            }
         }
     }
 }
